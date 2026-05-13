699873f0870a80d66dad2b95cadafec0dd503090
Gui2000-web <guilhermegomesalvarez@gmail.com>
2026-05-12 21:35:41 -0300

    Inicia repositório da aula12 - gitignore - venv

---
+++ aula12/comandos.py
@@ -0,0 +1,12 @@
+
+# python -m venv meu ambiente
+# source ./venv/Scripts/activate
+# Instalar as bibliotecas:
+# pip install pandas......
+# deactivate
+# rm -rf venv
+
+
+# Usar o arquivo requirements p/ fazer a instalação das bibliotecas
+# python -m venv venv
+# pip install -r requirements.txt
